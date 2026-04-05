831a95dc5aa39407269501a1b3eb7c9b721b3df1
GitHub Action <action@github.com>
2026-04-05 14:38:38 +0000

    Sync from company repo

--- CeyPASSCihazPanel.UI/Properties/AssemblyInfo.cs
+++ CeyPASSCihazPanel.UI/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.2.4")]
-[assembly: AssemblyFileVersion("1.2.2.4")]
+[assembly: AssemblyVersion("1.2.2.6")]
+[assembly: AssemblyFileVersion("1.2.2.6")]
